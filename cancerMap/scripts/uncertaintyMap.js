4b925d49dd601deec34f3553e15aa085dc2fb65f
Phil Gough <phil.j.gough@gmail.com>
2016-08-18 13:19:04 +1000

    start building the cancer map for the focus group research

---
+++ cancerMap/scripts/uncertaintyMap.js
@@ -0,0 +1,31 @@
+// 
+// 
+// 
+// 		D3 Cancer Atlas research
+// 			Code: Phil Gough <phillip.gough@qut.edu.au>
+// 			Design: Jessie Roberts, Phil Gough
+// 			
+// 			
+// 
+// 
+
+
+// append an svg to the .vis div
+var svg = d3.select(".vis").append("svg");
+
+// extract the dimensions of the div element
+var svgDimensions = d3.select(".vis").node().getBoundingClientRect();
+
+// define the internal padding of the svg
+var svgPadding = {
+	"top" : 10,
+	"bottom" : 10,
+	"left" : 10,
+	"right" : 10
+}
+
+
+
+
+
+
