467c2ef7bd42361c1454d3f56fa3332a1e8fd1c9
Phil Gough <phillip.gough@qut.edu.au>
2016-11-15 02:57:06 +1000

    monday afternoon push

--- pirate_game/app.py
+++ pirate_game/app.py
@@ -260,3 +260,13 @@ def try_again():
 
 if __name__ == '__main__':
    app.run(debug = True)
+
+
+
+
+# import os
+# from project import main
+
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 5000))
+#     main.app.run(host='0.0.0.0', port=port)
